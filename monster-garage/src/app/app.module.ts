f2f7a977a9b2bf92fbb4a982cc0d4ea25fe3fdf1
kzadlo <lolnik123>
2018-04-10 15:33:02 +0200

    Firebase connection was created

--- monster-garage/src/app/app.module.ts
+++ monster-garage/src/app/app.module.ts
@@ -1,22 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { ContentComponent } from './content/content.component';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {MenuComponent} from './menu/menu.component';
+import {ContentComponent} from './content/content.component';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {environment} from "../environments/environment";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-    ContentComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MenuComponent,
+        ContentComponent
+    ],
+    imports: [
+        BrowserModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- monster-garage/src/environments/environment.prod.ts
+++ monster-garage/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyD3Y4zcLP2rpw-Qrh06stFxSyiY2Wx8xo0",
+        authDomain: "monstergarage-4cdc9.firebaseapp.com",
+        databaseURL: "https://monstergarage-4cdc9.firebaseio.com",
+        projectId: "monstergarage-4cdc9",
+        storageBucket: "",
+        messagingSenderId: "877649468413"
+    }
 };
