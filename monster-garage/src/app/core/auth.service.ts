f62de8afd42f214bda224a7dec43cee5f3d67ebf
Krzysztof Urbańczyk <urbanczykkr@gmail.com>
2018-04-13 11:16:48 +0200

    fix adding to db

--- monster-garage/src/app/core/auth.service.ts
+++ monster-garage/src/app/core/auth.service.ts
@@ -28,7 +28,7 @@ export class AuthService {
     this.user = this.afAuth.authState
       .switchMap(user => {
         if (user) {
-          return this.afs.doc<User>('users/${user.uid}').valueChanges();
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
         } else {
           return Observable.of(null);
         }
@@ -53,7 +53,7 @@ export class AuthService {
   }
 
   private updateUserData(user) {
-    const userRef: AngularFirestoreDocument<User> = this.afs.doc('users/${user.uid}');
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
 
     const data: User = {
       uid: user.uid,
