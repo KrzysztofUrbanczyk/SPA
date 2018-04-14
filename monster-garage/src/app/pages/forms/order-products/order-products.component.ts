d0e562206a474644857e9ce688d50310fae0e5b5
Wojciech <wwnek429@gmail.com>
2018-04-14 10:31:02 +0200

    Temporary changes

--- monster-garage/src/app/pages/forms/order-products/order-products.component.ts
+++ monster-garage/src/app/pages/forms/order-products/order-products.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NbMenuItem } from '@nebular/theme';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-order-products',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderProductsComponent implements OnInit {
 
-  constructor() { }
+  public db: AngularFirestore;
+  private owner: string;
+  private newCategory: string;
+  private newCompany: string;
+  private newCompanyEmail: string;
+
+  public categories: Observable<{}>;
+  private companies: Observable<{}>;
+
+  private categoriesCollection: AngularFirestoreCollection<any>;
+  private companiesCollection: AngularFirestoreCollection<any>;
+  private orderCollection: AngularFirestoreCollection<any>;
+
+  private selectedCategory: string;
+  private selectedCompany: string;
+  private productName: string;
+  private quantity: string;
+
+  constructor(db: AngularFirestore) {
+    this.owner = 'root';
+    this.categoriesCollection = db.collection('Categories');
+    this.companiesCollection = db.collection('Companies');
+    this.orderCollection = db.collection('Orders');
+
+    this.categories = this.categoriesCollection.doc(this.owner).collection('Category').valueChanges();
+    this.companies = this.companiesCollection.doc(this.owner).collection('Company').valueChanges();
+   }
 
   ngOnInit() {
+  }
+
+  addNewCategory() {
+    this.categoriesCollection.doc(this.owner).collection('Category').doc(this.newCategory).set({
+      name: this.newCategory
+    });
+  }
+
+  addNewCompany() {
+    this.companiesCollection.doc(this.owner).collection('Company').doc(this.newCompany).set({
+       name: this.newCompany,
+       eMail: this.newCompanyEmail
+    });
+  }
+
+  sendOrder() {
+    this.orderCollection.doc(this.owner).collection('Order').add({
+      category: this.selectedCategory,
+      product: this.productName,
+      quantity: this.quantity,
+      company: this.selectedCompany
+    });
   }
 
 }
