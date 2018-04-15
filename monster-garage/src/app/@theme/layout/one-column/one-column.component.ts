389e952a0e0856c9a2a2e32feb732a49083af257
Krzysztof <urbanczykkr@gmail.com>
2018-04-15 14:22:51 +0200

    fix user position

--- monster-garage/src/app/@theme/layout/one-column/one-column.component.ts
+++ monster-garage/src/app/@theme/layout/one-column/one-column.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-one-column',
   templateUrl: './one-column.component.html',
-  styles: [],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./one-column.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class LayoutOneColumnComponent implements OnInit {
 
