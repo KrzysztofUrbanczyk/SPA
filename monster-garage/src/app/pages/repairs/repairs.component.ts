ba1c309721b87906c64eaad3e19f4fa1ced5f973
kzadlo <KrystianZadlo@fejm.pl>
2018-04-24 13:13:15 +0200

    Created collection of repairs

--- monster-garage/src/app/pages/repairs/repairs.component.ts
+++ monster-garage/src/app/pages/repairs/repairs.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Repairs } from './repairs';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-repairs',
   template: '',
   styles: []
 })
-export class RepairsComponent implements OnInit {
 
-  constructor() { }
+export class RepairsComponent implements OnInit {
+  repairs: Observable<Repairs[]>;
+  private repairsCollection: AngularFirestoreCollection<Repairs>;
+
+  constructor(private afs: AngularFirestore) {
+    this.repairsCollection = afs.collection<Repairs>('repairs');
+    this.repairs = this.repairsCollection.valueChanges();
+  }
 
   ngOnInit() {
   }
