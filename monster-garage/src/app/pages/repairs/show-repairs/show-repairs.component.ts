f0f485d89c1a1c0b559ace3ab1c267afab26d489
kzadlo <KrystianZadlo@fejm.pl>
2018-04-25 22:26:03 +0200

    Show-repairs component was generated

---
+++ monster-garage/src/app/pages/repairs/show-repairs/show-repairs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-repairs',
+  templateUrl: './show-repairs.component.html',
+  styleUrls: ['./show-repairs.component.css']
+})
+export class ShowRepairsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
