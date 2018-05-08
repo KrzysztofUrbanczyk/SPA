d7b14612cb67bf1778705399f31f2e31c552f553
kzadlo <KrystianZadlo@fejm.pl>
2018-05-08 15:55:34 +0200

    Added validation to profil page and updated view

--- monster-garage/src/app/pages/forms/user-profile/user-profile.component.ts
+++ monster-garage/src/app/pages/forms/user-profile/user-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../core/auth.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ToastrController } from 'ng2-toastr-notifications';
 
 @Component({
   selector: 'app-user-profile',
@@ -8,29 +10,52 @@ import { AuthService } from '../../../core/auth.service';
 })
 export class UserProfileComponent implements OnInit {
   user: any;
-  displayName: string;
-  email: string;
+  editForm: FormGroup;
+  displayName: FormControl;
+  email: FormControl;
   photoURL: string;
   uid: string;
 
-  constructor(public auth: AuthService) {
+  constructor(public auth: AuthService,
+              private toastCtrl: ToastrController) {
   }
 
   ngOnInit() {
     this.auth.user.subscribe((user: any) => this.user = user);
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createFormControls() {
+    this.displayName = new FormControl('', [
+      Validators.minLength(3)
+    ]);
+  }
+
+  createForm() {
+    this.editForm = new FormGroup({
+      displayName: this.displayName,
+    });
   }
 
   updateData() {
-    this.uid = this.user.uid;
-    this.email = this.user.email;
-    this.photoURL = this.user.photoURL;
-    this.user = {
-      uid: this.uid,
-      email: this.email,
-      displayName: this.displayName,
-      photoURL: this.photoURL
-    };
-    this.auth.updateUserData(this.user);
+    if (this.editForm.valid) {
+      this.uid = this.user.uid;
+      this.email = this.user.email;
+      this.photoURL = this.user.photoURL;
+      this.user = {
+        uid: this.uid,
+        email: this.email,
+        displayName: this.displayName.value,
+        photoURL: this.photoURL
+      };
+      this.auth.updateUserData(this.user);
+      this.toastCtrl.show({
+        type: 'success',
+        title: 'Sukces',
+        message: 'Dokonano aktualizacji danych'
+      });
+    }
   }
 
 }
